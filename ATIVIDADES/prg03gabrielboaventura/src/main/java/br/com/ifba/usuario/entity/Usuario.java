a3bad7548253b01f37fa64525c93ed37f1966dfd
gabrielboaventura2017-tech <gabrielboaventura2017@gmail.com>
2026-09-16 16:25:26 -0300

    encapsula classe usuario e integra getters e setters

--- ATIVIDADES/prg03gabrielboaventura/src/main/java/br/com/ifba/usuario/entity/Usuario.java
+++ ATIVIDADES/prg03gabrielboaventura/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -9,8 +9,47 @@ package br.com.ifba.usuario.entity;
  * @author gabri
  */
 public class Usuario {
+    
     // Dados do usuário
-    public String nome;
-    public String email;
-    public String senha;
+    private String nome;
+    private String email;
+    private String senha;
+    
+    // Construtor vazio
+    public Usuario() {
+    }
+
+    // Construtor com os dados do usuário
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+    
+    // Métodos de acesso do nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Métodos de acesso do e-mail
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Métodos de acesso da senha
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
